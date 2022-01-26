fff1f6232a7bb7fb60786f344c6fc3e149ca0b1d
mayankj2018 <98470674+mayankj2018@users.noreply.github.com>
2022-01-26 20:17:45 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,20 @@
+#Add implementation
+def add(x,y):
+	return x+y
+
+#Subtract implementation
+def subtract(x,y):
+	return x-y #on master
+
+#Multiply implementation
+def multiply(x,y):
+	return x*y # on bug456
+
+#Divide implementation
+def divide(x,y):
+	return x/y # on master
+
+#square implementation
+def square(x):
+	return x*x
+

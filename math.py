f4a8643d890cbbbaeff88e476556b384ea8e3765
Mayank Jain <mayank_jain6@persistent.com>
2022-01-26 20:20:35 +0530

    Added double fn headers

--- math.py
+++ math.py
@@ -1,20 +1,20 @@
-#Add implementation
+#Add implementation 2
 def add(x,y):
 	return x+y
 
-#Subtract implementation
+#Subtract implementation 2
 def subtract(x,y):
 	return x-y #on master
 
-#Multiply implementation
+#Multiply implementation 2
 def multiply(x,y):
 	return x*y # on bug456
 
-#Divide implementation
+#Divide implementation 2
 def divide(x,y):
 	return x/y # on master
 
-#square implementation
+#square implementation 2
 def square(x):
 	return x*x
 
